5f44c01cf3320659e114071e4b562450d3f23e52
Muhammad Yasir Malkani <muhammadyasirmalkani1@gmail.com>
2025-08-02 22:53:24 +0500

    Update CareersPage.tsx

--- src/pages/CareersPage.tsx
+++ src/pages/CareersPage.tsx
@@ -1,1 +1,9 @@
-import
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { ShieldCheck, Shield, Lock, Eye, Database, FileText, Server, UserCheck } from 'lucide-react';
+import HeroSection from '../components/sections/HeroSection';
+import ServiceCard from '../components/cards/ServiceCard';
+import ComplianceCard from '../components/cards/ComplianceCard';
+import IndustryCard from '../components/cards/IndustryCard';
+import FeatureItem from '../components/elements/FeatureItem';
+import CTASection from '../components/sections/CTASection';
